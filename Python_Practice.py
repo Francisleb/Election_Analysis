5c227375396ff4da45e509f0277855df0f02f7f3
Francis L Dorwu <lebened@yahoo.com>
2020-08-02 21:18:50 +0000

    Adding the election audit code.

---
+++ Python_Practice.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+Counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in Counties:
+    print("El Paso is in list of counties")
+else:
+    print("El Paso is not in list of counties")
